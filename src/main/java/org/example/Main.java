0ad5a3bc361da52d4f251ea0f05252213f3ce29c
JoHyungJun <jhj8371@naver.com>
2023-12-28 23:02:49 +0900

    [Feat #1] 커밋 메세지 테스트 - 테스트 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        System.out.println("commit test");
+        System
+        .out
+        .println
+        ("Hello world!!!!");
     }
 }
